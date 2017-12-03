458897a4a38d5a8d27422fc186e7a1b5eae47c19
rustem-s <rustem.saidaliyev@gmail.com>
2017-12-03 13:10:29 +0300

    Добавил выделение цветом мин. и макс. значений курсов

--- client/src/app/table/table.component.ts
+++ client/src/app/table/table.component.ts
@@ -9,9 +9,14 @@ import {Rate} from "../rate";
 })
 export class TableComponent implements OnInit {
 
-  rates: Rate[]=[];
+    rates: Rate[]=[];
 
-  constructor(private dataService: DataService) { }
+    minUsd: number = 9999999;
+    maxUsd: number = 0;
+    minEur: number = 9999999;
+    maxEur: number = 0;
+
+    constructor(private dataService: DataService) { }
 
   ngOnInit() {
   }
@@ -20,8 +25,22 @@ export class TableComponent implements OnInit {
 
     this.dataService.getData().subscribe(
         data => {
-          this.rates = data;
-          console.log(this.rates)
+            this.rates = data;
+
+            for (var i = 0; i < this.rates.length; i++) {
+                if (this.rates[i].usd < this.minUsd ) {
+                    this.minUsd = this.rates[i].usd;
+                }
+                if (this.rates[i].usd > this.maxUsd ) {
+                    this.maxUsd = this.rates[i].usd;
+                }
+                if (this.rates[i].eur < this.minEur ) {
+                    this.minEur = this.rates[i].eur;
+                }
+                if (this.rates[i].eur > this.maxEur ) {
+                    this.maxEur = this.rates[i].eur;
+                }
+            }
         },
         err => {
           console.log("Error occured")
